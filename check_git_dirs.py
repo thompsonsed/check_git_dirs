49d6f86847f52c3ad5ebf7dab8c55eebc091e273
Sam Thompson <thompsonsed@gmail.com>
2020-05-27 09:11:35 +0100

    Updated colouring of formats

--- check_git_dirs.py
+++ check_git_dirs.py
@@ -6,16 +6,24 @@ Prints the output of git rev-parse
 import pathlib
 from subprocess import check_output, call
 
-
 if __name__ == "__main__":
-    git_dirs = []
     for f in check_output(["find", ".", "-name", ".git"]).decode("utf-8").splitlines():
         git_dir = pathlib.Path(f).parent
         rev_parse = check_output(["git", "status", "-s"], cwd=git_dir).decode("utf-8").splitlines()
-        print(f"\033[1;31m{git_dir}:")
+        output_message = f"\033[1;34m{git_dir}: "
+        if len(rev_parse) == 0:
+            git_status = check_output(["git", "status", "-s"], cwd=git_dir).decode("utf-8")
+            if "Your branch is ahead" in git_status:
+                output_message += "\033[1;33m - Requires push"
+            else:
+                output_message += "\033[1;32m \u2713 All up to date"
+        else:
+            output_message += "\033[1;31m \u2717 Unstaged changes"
+        print(output_message)
         for r in rev_parse[0:10]:
-            print(f"\t\033[1;34m{r}")
-    # find . -name .git -execdir bash -c 'echo -en "\033[1;31m"repo: "\033[1;34m"; basename "`git rev-parse --show-topl>
+            print(f"\t\033[1;31m{r}")
+        if len(rev_parse) > 10:
+            print("\t\033[1;31m....")
 
 
 
