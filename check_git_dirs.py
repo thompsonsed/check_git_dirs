01af0b4a9c7e4e5e456a2f645b6be7d6a5f2a2d0
thompsonsed <thompsonsed@gmail.com>
2021-08-04 11:00:31 +0100

    Adds outputting of a list of the repositories that are unstaged or require a push

--- check_git_dirs.py
+++ check_git_dirs.py
@@ -3,7 +3,7 @@ Checks the status of all git directories in the current directory.
 
 Prints the output of git rev-parse
 """
-from typing import Tuple
+from typing import Tuple, List
 import pathlib
 import argparse
 from subprocess import check_output, call
@@ -17,11 +17,14 @@ def add_branch_label(record_branches: bool, git_status: str, output_message: str
     return output_message
 
 
-def scan_all_git_repos(directory: pathlib.Path, record_branches: bool = False) -> Tuple[int, int, int]:
+def scan_all_git_repos(
+    directory: pathlib.Path, record_branches: bool = False
+) -> Tuple[int, int, int, List[str], List[str]]:
     """
     Checks all git repositories in the path for those requiring an action.
 
-    :return: a tuple of the number of repositories with no changes, push required or unstaged changes
+    :return: a tuple of the number of repositories with no changes, push required or unstaged changes,
+             a list of the unstaged repos and a list of the repos required a push
     """
     if not directory.exists():
         raise IOError(f"Directory does not exist at {directory}")
@@ -30,6 +33,8 @@ def scan_all_git_repos(directory: pathlib.Path, record_branches: bool = False) -
     total_push = 0
     total_okay = 0
     total_unstaged = 0
+    unstaged_list = []
+    push_list = []
     for f in git_dirs:
         git_dir = pathlib.Path(f).parent
         rev_parse = check_output(["git", "status", "-s"], cwd=git_dir).decode("utf-8").splitlines()
@@ -39,6 +44,7 @@ def scan_all_git_repos(directory: pathlib.Path, record_branches: bool = False) -
             if "Your branch is ahead" in git_status:
                 output_message += "\033[1;33m - Requires push"
                 total_push += 1
+                push_list.append(git_dir)
             else:
                 output_message += "\033[1;32m \u2713"
                 total_okay += 1
@@ -51,11 +57,12 @@ def scan_all_git_repos(directory: pathlib.Path, record_branches: bool = False) -
                 add_branch_label(record_branches=record_branches, git_status=git_status, output_message=output_message)
             )
             total_unstaged += 1
+            unstaged_list.append(git_dir)
         for r in rev_parse[0:10]:
             logging.warning(f"\t\033[1;31m{r}")
         if len(rev_parse) > 10:
             logging.warning("\t\033[1;31m....")
-    return total_unstaged, total_push, total_okay
+    return total_unstaged, total_push, total_okay, unstaged_list, push_list
 
 
 def dir_path(p: str):
@@ -94,13 +101,17 @@ if __name__ == "__main__":
         logging.getLogger().setLevel(20)
     else:
         logging.getLogger().setLevel(30)
-    total_unstaged, total_push, total_okay = scan_all_git_repos(pathlib.Path(args.dir), args.branch)
+    total_unstaged, total_push, total_okay, unstaged_list, push_list = scan_all_git_repos(
+        pathlib.Path(args.dir), args.branch
+    )
     grand_total = total_unstaged + total_push + total_okay
     logging.warning("\033[1;32mChecks completed")
     if total_unstaged != 0:
         logging.warning(f"\033[1;31mUnstaged changes: {total_unstaged}")
+        logging.info(f"\033[1;31m{', '.join([str(x) for x in unstaged_list])}")
     if total_push != 0:
         logging.warning(f"\033[1;33mRequires push: {total_push}")
+        logging.info(f"\033[1;33m{', '.join([str(x) for x in push_list])}")
     if grand_total == total_okay:
         logging.warning(f"\033[1;32mAll {total_okay} repositories okay.")
     else:
